05a1b4c835063a73e932a5f31563dca6b4916db2
André Gaul <gaul@web-yard.de>
2013-07-27 20:35:43 +0200

    add YesornoView

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -6,27 +6,35 @@ define(["jquery", "underscore", "backbone", "jquerymobile"],
       initialize: function() {
         this.firstPage = true;
         $.mobile.initializePage();
-        Backbone.history.start();
         // display page when JQM finished its magic.
         $(document.body).css('visibility','');
       },
       routes: {
         "": "home",
-        "home": "home"
       },
       home: function() {
-        this.changePage("#home");
+        // TODO: get actual initial ID from domain
+        // (e.g. istemmaschonda.yesorno.it)
+        var id = 'istemmaschonda';
+
+        showYesornoPage(id);
       },
-      changePage: function(pageid) {
-        console.log('change to page '+pageid);
+      changePage: function(page) {
+        console.log('change page');
+
+        // prepare page
+        $(page.el).attr('data-role', 'page');
+        page.render();
+        $('body').append($(page.el));
+
+        // make transition / actually change page
         var transition = this.firstPage ? "none" : $.mobile.defaultPageTransition;
         this.firstPage = false;
-        $.mobile.changePage(pageid, { transition: transition, changeHash: false });
+        $.mobile.changePage($(page.el), { transition: transition, changeHash: false });
       }
     });
 
-    var Yesorno = Backbone.model.extend({
-
+    var Yesorno = Backbone.Model.extend({
       // validate() should check the same fields as validate_doc_update()
       // in the CouchDB design document
       validate: function(attributes) {
@@ -37,6 +45,32 @@ define(["jquery", "underscore", "backbone", "jquerymobile"],
       }
     });
 
+    var YesornoView = Backbone.View.extend({
+      template: _.template( $("#template_YesornoPage").html() ),
+      render: function() {
+        $(this.el).html( this.template( this.model.toJSON() ) );
+      }
+    });
+
     this.router = new MnemeRouter();
+
+    function showYesornoPage(id) {
+      // TODO retrieve the actual yesorno doc here instead of creating a
+      // fixed one.
+      var yesorno = new Yesorno({
+        question: 'Ist Emma schon da?',
+        answer: 'Noooain!'
+      });
+
+      // change page to fresh view associated with model 'yesorno'
+      this.router.changePage(
+        new YesornoView({
+          model: yesorno,
+          el: $('<div></div>')
+        })
+      );
+    }
+
+    Backbone.history.start();
   }
 );
